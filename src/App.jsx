0315d908ea632af6ae177f91867b37cc7aed356c
henriquemha2 <157602258+henriquemha2@users.noreply.github.com>
2024-01-25 15:12:18 +0000

    Error :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="card">
-      <h1>Rick Sanchez</h1>
-          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-    </>
+  <>
+     <Card />
+     <Card />
   )
 }
-
 export default App
